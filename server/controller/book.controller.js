8a629176bd6e59434e4298f5267ae0cfd87383de
haricharan61108 <116500176+haricharan61108@users.noreply.github.com>
2024-09-05 13:52:47 +0530

    added the login page and changed navbar

---
+++ server/controller/book.controller.js
@@ -0,0 +1,39 @@
+import { PrismaClient } from "@prisma/client";
+import bcrypt from "bcryptjs";
+
+const prisma=new PrismaClient();
+
+export const bookBook=async(req,res)=> {
+  const {user_id,book_id}=req.body;
+
+  try {
+    const book=await prisma.book.findUnique({
+        where: {
+            book_id
+        }
+    });
+
+    if(book && book.copies_available>0) {
+        const borrowedBook=await prisma.borrowedBook.create({
+            data: {
+                user_id,
+                book_id,
+                borrow_date:new Date(),
+                due_date:new Date(date.now()+14*24*60*60*1000)
+            }
+        })
+        await prisma.book.update({
+            where:{book_id},
+            data: {
+                copies_available:book.copies_available-1
+            }
+        });
+        res.status(200).json({msg:"book borrowed succesfully",borrowedBook})
+    }
+    else {
+        res.status(200).json({error:"book is not available"});
+    }
+  } catch (error) {
+    res.status(400).json({error:"Error during booking"})
+  }
+}
